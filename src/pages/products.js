4802ed6978b080124225166eb2673bc4b87a32c1
Oren Raboy <oraboy@gmail.com>
2018-12-07 01:11:32 +0200

    force build

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,20 +1,19 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import { SSL_OP_TLS_BLOCK_PADDING_BUG } from "constants";
 
 export default ({ data }) => {
   console.log(data)
   return (
     <Layout>
-      <div>Hello wo rld -- {data.site.siteMetadata.title}</div>
+      <div>Hello <b>world</b> -- {data.site.siteMetadata.title}</div>
         <table>
         <tbody>
             {data.allMongodbNanoaffilGiftIdeas.edges.map(({ node }, index) => (
                 <tr key={index}>
                     <td>{node.id}</td>
                     <td>{node.name}</td>
-                    <td>{node.affiliateLink}</td>
+                    <td>XXX {node.affiliateLink}</td>
                     <td>{node.productionData}</td>
                 </tr>
                 ))}
