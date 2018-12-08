0b1561748fa04d2f2a02d74491f405e2f8c6502e
Oren Raboy <oraboy@gmail.com>
2018-12-08 09:39:36 +0200

    n1

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,11 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Image from '../components/image'
 
-const IndexPage = () => (
+const IndexPage = (data) => (
   <Layout>
-    <h1>Product Recommendations Galore!! 🎁</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Product Recommendations Galore!!</h1>
+
+      
   </Layout>
 )
 
--- src/pages/products.js
+++ src/pages/products.js
@@ -19,6 +19,23 @@ export default ({ data }) => {
                 ))}
         </tbody>
         </table>
+        <p>IT:</p>
+
+<script type="text/javascript">
+amzn_assoc_placement = "adunit0";
+amzn_assoc_tracking_id = "thebestgift0a-20";
+amzn_assoc_ad_mode = "manual";
+amzn_assoc_ad_type = "smart";
+amzn_assoc_marketplace = "amazon";
+amzn_assoc_region = "US";
+amzn_assoc_linkid = "b0ba9863ac1fff204db18230baf0a46e";
+amzn_assoc_design = "in_content";
+amzn_assoc_asins = "B01EVKUKSG,B07J5Q4P38";
+amzn_assoc_title = "moshe leon";
+</script>
+<script src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US"></script>
+
+        <p>GULP........</p>
     </Layout>
   )
 }
